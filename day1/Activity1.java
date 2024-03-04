de0f12a103f6da582ce0de894ffb8e906ec7b7cb
kalaivanikumar950 <162138241+kalaivanikumar950@users.noreply.github.com>
2024-03-04 19:43:38 +0530

    Add files via upload

---
+++ day1/Activity1.java
@@ -0,0 +1,8 @@
+package week1.day1;
+
+public class Activity1 {
+   public static void main(String[] args) {
+	    System.out.println("welcome to my First Program!! ");
+}
+	
+}
---
+++ day1/Activity2.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Activity2 {
+	
+	public static void main(String[] args) {
+		
+	    	 float browserVersion= 100.2f;
+	    	 String browserName= "FireFox";
+			boolean isVisible = true;
+	    	 int releseYear = 1998;
+	    	 char browserLogo = 'f';
+	    	 System.out.println("Browser Name is" +browserName);
+	    	 System.out.println("Browser version is" +browserVersion);
+	    	 System.out.println("Browser releseYear is"+releseYear);
+	    	 System.out.println("Browser Logo is" +browserLogo);
+	    	 System.out.println("Browser is Visible   " +isVisible);
+	        
+	    	 
+	    	 
+
+	}
+
+}
